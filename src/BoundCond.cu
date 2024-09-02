206786e032d6a9744ec96c8e409d256af047d188
liang <15652586980@163.com>
2024-09-02 14:52:17 +0800

    将壁面的虚网格延伸出去，否则计算扰动时会出现错误

--- src/BoundCond.cu
+++ src/BoundCond.cu
@@ -381,25 +381,25 @@ void DBoundCond::link_bc_to_boundaries(Mesh &mesh, std::vector<Field> &field) co
     link_boundary_and_condition(mesh[i].boundary, back_pressure_info, n_back_pressure, i_back_pressure, i);
     link_boundary_and_condition(mesh[i].boundary, periodic_info, n_periodic, i_periodic, i);
   }
-  for (auto i = 0; i < n_block; i++) {
-    for (size_t l = 0; l < n_wall; l++) {
-      const auto nb = wall_info[l].n_boundary;
-      for (size_t m = 0; m < nb; m++) {
-        auto i_zone = wall_info[l].boundary[m].x;
-        if (i_zone != i) {
-          continue;
-        }
-        auto &b = mesh[i].boundary[wall_info[l].boundary[m].y];
-        for (int q = 0; q < 3; ++q) {
-          if (q == b.face) continue;
-          b.range_start[q] += ngg;
-          b.range_end[q] -= ngg;
-        }
-      }
-    }
-    cudaMemcpy(field[i].h_ptr->boundary, mesh[i].boundary.data(), mesh[i].boundary.size() * sizeof(Boundary),
-               cudaMemcpyHostToDevice);
-  }
+//  for (auto i = 0; i < n_block; i++) {
+//    for (size_t l = 0; l < n_wall; l++) {
+//      const auto nb = wall_info[l].n_boundary;
+//      for (size_t m = 0; m < nb; m++) {
+//        auto i_zone = wall_info[l].boundary[m].x;
+//        if (i_zone != i) {
+//          continue;
+//        }
+//        auto &b = mesh[i].boundary[wall_info[l].boundary[m].y];
+//        for (int q = 0; q < 3; ++q) {
+//          if (q == b.face) continue;
+//          b.range_start[q] += ngg;
+//          b.range_end[q] -= ngg;
+//        }
+//      }
+//    }
+//    cudaMemcpy(field[i].h_ptr->boundary, mesh[i].boundary.data(), mesh[i].boundary.size() * sizeof(Boundary),
+//               cudaMemcpyHostToDevice);
+//  }
   for (int i = 0; i < n_wall; i++) {
     delete[]i_wall[i];
   }
