d25f3d19eb62079d2ecedb4392afea07cf878ee4
liang <15652586980@163.com>
2024-11-17 16:33:22 +0800

    delete unused functions

--- src/DigitalFilter.cuh
+++ src/DigitalFilter.cuh
@@ -15,14 +15,6 @@ struct DZone;
 
 struct Inflow;
 
-void initialize_digital_filter(Parameter &parameter, cfd::Mesh &mesh, DBoundCond &dBoundCond);
-
-// Steps to initiate the digital filter
-
-// 1. Compute the Lund matrix for the digital filter
-void get_digital_filter_lund_matrix(cfd::Parameter &parameter, cfd::DBoundCond &dBoundCond, std::vector<int> &N1,
-                                    std::vector<std::vector<real>> &scaled_y);
-
 void assume_gaussian_reynolds_stress(cfd::Parameter &parameter, cfd::DBoundCond &dBoundCond, std::vector<int> &N1,
                                      std::vector<std::vector<real>> &y_scaled);
 
@@ -62,29 +54,4 @@ Castro_time_correlation_and_fluc_computation(ggxl::VectorField2D<real> *velFluc_
                                              int ngg, DParameter *param, DZone *zone, Inflow *inflow,
                                              ggxl::VectorField3D<real> *fluctuation_dPtr,
                                              ggxl::VectorField1D<real> *lundMatrix_dPtr);
-
-// 2. Compute the length scale for the digital filter
-// Currently ignore this part. For mixing layers, the value is just the initial vorticity thickness
-
-// 3. Compute the convolution kernel for the digital filter
-void get_digital_filter_convolution_kernel(cfd::Parameter &parameter, cfd::DBoundCond &dBoundCond, std::vector<int> &N1,
-                                           std::vector<std::vector<real>> &y_scaled, real dz);
-
-// 4. Initialize the random number states for the digital filter
-void initialize_digital_filter_random_number_states(cfd::DBoundCond &dBoundCond, std::vector<int> &N1,
-                                                    std::vector<int> &N2, int ngg);
-
-// 5. Compute the velocity fluctuations with convolution kernel
-void
-apply_convolution(cfd::Parameter &parameter, cfd::DBoundCond &dBoundCond, std::vector<int> &N1,
-                  std::vector<int> &N2, int ngg);
-
-// 7. compute the fluctuations!
-void compute_fluctuations(cfd::DBoundCond &dBoundCond, std::vector<int> &N1, std::vector<int> &N2, int ngg);
-
-
-__global__ void
-compute_fluctuations(ggxl::VectorField3D<real> *fluctuation_dPtr, ggxl::VectorField1D<real> *lundMatrix_dPtr,
-                     ggxl::VectorField2D<real> *velFluc_dPtr, int iFace, int my, int mz, int ngg);
-
 }
